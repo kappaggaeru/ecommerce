a58554e902587ff8f363c3bc02153f6707e82e50
kappaggaeru <oliveralautaro99@gmail.com>
2020-08-01 05:37:20 -0300

    first commit or whatever

---
+++ js/main.js
@@ -0,0 +1,84 @@
+"use strict";
+console.log("hello world");
+//elements
+let sideBar = document.querySelector(".sideBar");
+let searchBar = document.querySelector(".searchBar");
+let cart = document.querySelector(".cart");
+
+//buttons
+let openBurger = document.querySelector("#openBurger");
+let closeBurger = document.querySelector("#closeBurger");
+let openCart = document.querySelector("#openCart");
+let closeCart = document.querySelector("#closeCart");
+
+//acctions on ready
+sideBar.classList.add("closed");//empieza cerrada
+searchBar.classList.add("hidden");
+closeBurger.classList.add("hidden");
+cart.classList.add("closed");
+
+//sidebar interaction
+openBurger.addEventListener("click",function(){
+    openBurger.classList.toggle("hidden");
+    closeBurger.classList.toggle("hidden");
+    if(sideBar.classList.contains("closing"))sideBar.classList.toggle("closing");
+    sideBar.classList.toggle("opening");
+    sideBar.classList.toggle("closed");
+});
+closeBurger.addEventListener("click",function(){
+    closeBurger.classList.toggle("hidden");
+    openBurger.classList.toggle("hidden");
+    sideBar.classList.toggle("opening");
+    sideBar.classList.toggle("closing");
+    sideBar.classList.toggle("closed");
+});
+
+// searchbar interaction
+document.querySelector("#search").addEventListener("click",function(){
+    if(searchBar.classList.contains("hidden")){
+        searchBar.classList.remove("hidden");
+        searchBar.classList.remove("close");
+        searchBar.classList.add("open");
+    }else{
+        searchBar.classList.remove("open");
+        searchBar.classList.add("close");
+        searchBar.classList.add("hidden");
+    }
+});
+
+openCart.addEventListener("click",function(){
+    if(cart.classList.contains("closing"))cart.classList.toggle("closing");
+    cart.classList.toggle("opening");
+    cart.classList.toggle("closed");
+});
+closeCart.addEventListener("click",function(){
+    cart.classList.toggle("opening");
+    cart.classList.toggle("closing");
+    cart.classList.toggle("closed");
+});
+
+let footAbout = document.querySelector('div[name="footAboutDrop"]');
+let footInfo = document.querySelector('div[name="footInfoDrop"]');
+
+let imgFootAbout = document.querySelector('img[name="footImgAbout"]');
+let imgFootInfo = document.querySelector('img[name="footImgInfo"]');
+
+let whiteDropDown = "images/icons/dropDownWhite.png";
+let whiteDropUp = "images/icons/dropUpWhite.png";
+
+document.querySelector('div[name="footAbout"]').addEventListener("click",function(){
+    footAbout.classList.toggle("hidden");
+    if(footAbout.classList.contains("hidden")){
+        imgFootAbout.src = whiteDropDown;
+    }else{
+        imgFootAbout.src = whiteDropUp;
+    }
+});
+document.querySelector('div[name="footInfo"]').addEventListener("click",function(){
+    footInfo.classList.toggle("hidden");
+    if(footInfo.classList.contains("hidden")){
+        imgFootInfo.src = whiteDropDown;
+    }else{
+        imgFootInfo.src = whiteDropUp;
+    }
+});
---
+++ js/product.js
@@ -0,0 +1,43 @@
+let sizesDiv = document.querySelector('div[name="sizesDropdown"]');
+let sizesDrop = document.querySelector('img[name="sizesDrop"]');
+
+let descDiv = document.querySelector('div[name="descriptionDrowdown"]');
+let descDrop = document.querySelector('img[name="descDrop"]');
+
+let dropUp = "images/icons/dropUp.png";
+let dropDown = "images/icons/dropDown.png";
+
+let comboQuantity = document.querySelector('.quanInput');
+let quantity = 0;
+quantity = comboQuantity.value;
+
+document.querySelector('img[name="quantityMinus"]').addEventListener("click",function(){
+    if(quantity > 1){
+        quantity--;
+        comboQuantity.value = quantity;
+    }
+});
+document.querySelector('img[name="quantityPlus"]').addEventListener("click",function(){
+    if(quantity < 10){
+        quantity++;
+        comboQuantity.value = quantity;
+    }
+});
+
+document.querySelector('div[name="toggleSizes"]').addEventListener("click", function(){
+    sizesDiv.classList.toggle("hidden");
+    if(sizesDiv.classList.contains("hidden")){
+        sizesDrop.src = dropDown;
+    }else{
+        sizesDrop.src = dropUp;
+    }
+});
+
+document.querySelector('div[name="toggleDesc"]').addEventListener("click", function(){
+    descDiv.classList.toggle("hidden");
+    if(descDiv.classList.contains("hidden")){
+        descDrop.src = dropDown;
+    }else{
+        descDrop.src = dropUp;
+    }
+});
